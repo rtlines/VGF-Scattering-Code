39f858621bf5784e1a92de6e74778f6c57a00a71
rtlines <linest@byui.edu>
2022-02-07 16:27:42 -0700

    got the read_intput_file.py and read_kv.py files to work and put them into vgffmc_lines.py as functions.

--- pycode/read_input_file.py
+++ pycode/read_input_file.py
@@ -4,34 +4,40 @@ workfile = "test_output_file.out"
 
 with open(workfile) as f:
      read_data = f.readline()            # read a whole line
-     print(read_data)
+#     print(read_data)
      read_data = f.readline()            # read a whole line
-     print(read_data)
+#     print(read_data)
      NUSE = int(read_data)
-     print(NUSE)
+     #print(NUSE)
      read_data = f.readline()            # read a whole line
      split_string = read_data.split(" ")  #parse the line splitting where
-     print(split_string)
+     #print(split_string)
      W = float(split_string[3])
      alpha = float(split_string[10])
      beta = float(split_string[17])
      gamma = float(split_string[31])
      psi = float(split_string[38])
-     print(W,alpha,beta,gamma,psi)
+     #print(W,alpha,beta,gamma,psi)
      read_data = f.readline()            # read a whole line
      split_string = read_data.split(" ")  #parse the line splitting where
-     print(split_string)  
+     #print("  ")
+     #print(split_string)  
      ER = float(split_string[3])
      EI = float(split_string[9])
-     TD = float(split_string[16])
-     print(ER, EI, TD)
+     TD = float(split_string[15])
+     #print(ER, EI, TD)
      # make a place for the locations of the dipole cells, we need x,y,z components
      # and make a place for the dipole weights
      R = np.zeros((NUSE,3))
      D = np.zeros(NUSE)
      for i in range(0,NUSE,1):             # loop to input all the theta and phi values
           read_data = f.readline()       # read in a whole line
-          #print(read_data)
+          #print("read_data",read_data)
+          while "  " in read_data:        # make double spaces into sinble spaces
+              read_data = read_data.replace("  "," ")
+          read_data.lstrip()           # remove leading space
+          read_data.lstrip()           # remove leading space
+          #print("read_data",read_data)
           split_string = read_data.split(" ")  #parse the line splitting where
                                                #there are spaces
                                                #Each line starts with a space
@@ -42,11 +48,20 @@ with open(workfile) as f:
                                                #a space, a string that is a 
                                                #number, then a second string 
                                                #that is a number, etc. 
-          print(split_string)
-          R[i][0] = float(split_string[3])
-          R[i][1] = float(split_string[5])
-          R[i][2] = float(split_string[8])
-          D[i] = float(split_string[11])
+          #print("split_string1",split_string)
+          R[i][0] = float(split_string[1])
+          R[i][1] = float(split_string[2])
+          R[i][2] = float(split_string[3])
+          D[i] = float(split_string[4])
           #still need 
+     read_data = f.readline()            # read a whole line
+     #print(read_data)
+     temp = float(read_data)
+     #print(temp)
+     read_data = f.readline()            # read a whole line
+     #print(read_data)
+     factor = float(read_data) 
+     #print(factor)
+     
           
           
--- pycode/vgffmc_lines.py
+++ pycode/vgffmc_lines.py
@@ -16,8 +16,7 @@ import matplotlib.pyplot as plt
 ###############################################################################
 #
 ###############################################################################
-def read_input_file(workfile, comments, NUSE, wave, alpha,beta,gamma,psi,RAD, ER, EI,
-            TD, R, D ):
+def read_input_file(workfile):
    # comments is the coment string from vgfin
    # NUSE is the number of dipoles to use
    # wave is the wavelength in relative uints (as long as all lenght units 
@@ -33,51 +32,72 @@ def read_input_file(workfile, comments, NUSE, wave, alpha,beta,gamma,psi,RAD, ER
    # Start reading in data
    print("start read intput file")
    with open(workfile) as f:
+        #print("read comment")
         read_data = f.readline()            # read a whole line
-        print(read_data)
+        com=read_data
+        #print(com)
         read_data = f.readline()            # read a whole line
-        print(read_data)
+        #print("NUSE",read_data)
         NUSE = int(read_data)
-        print(NUSE)
+        #print("NUSE", NUSE)
         read_data = f.readline()            # read a whole line
         split_string = read_data.split(" ")  #parse the line splitting where
-        print(split_string)
-        W = float(split_string[3])
+        #print(split_string)
+        wave = float(split_string[3])
         alpha = float(split_string[10])
         beta = float(split_string[17])
         gamma = float(split_string[31])
         psi = float(split_string[38])
-        print(W,alpha,beta,gamma,psi)
+        #print("W,alpha, beta, gamma, psi", W,alpha,beta,gamma,psi)
         read_data = f.readline()            # read a whole line
         split_string = read_data.split(" ")  #parse the line splitting where
-        print(split_string)  
+        #print("  ")
+        #print(split_string)  
         ER = float(split_string[3])
         EI = float(split_string[9])
-        TD = float(split_string[16])
-        print(ER, EI, TD)
+        TD = float(split_string[15])
+        #print("ER, EI, TD", ER, EI, TD)  
         # make a place for the locations of the dipole cells, we need x,y,z components
         # and make a place for the dipole weights
-        R = np.zeros((NUSE,3))
+        R = np.zeros((NUSE,3))  
         D = np.zeros(NUSE)
         for i in range(0,NUSE,1):             # loop to input all the theta and phi values
-             read_data = f.readline()       # read in a whole line
-             #print(read_data)
-             split_string = read_data.split(" ")  #parse the line splitting where
-                                                  #there are spaces
-                                                  #Each line starts with a space
-                                                  #then a number, then a space
-                                                  #then a vextor and then a weight.
-                                                  # So we will get
-                                                  # severak parts to our split
-                                                  #a space, a string that is a 
-                                                  #number, then a second string 
-                                                  #that is a number, etc. 
-             print(split_string)
-             R[i][0] = float(split_string[3])
-             R[i][1] = float(split_string[5])
-             R[i][2] = float(split_string[8])
-             D[i] = float(split_string[11])
-          #still need 
+           #print("I = ",i," NUSE = ", NUSE)
+           read_data = f.readline()       # read in a whole line
+           #print("read_data in loop",read_data)
+           while "  " in read_data:        # make double spaces into sinble spaces
+               read_data = read_data.replace("  "," ")
+           read_data.lstrip()           # remove leading space
+           read_data.lstrip()           # remove leading space
+           #print("read_data",read_data)
+           split_string = read_data.split(" ")  #parse the line splitting where
+                                                #there are spaces
+                                                #Each line starts with a space
+                                                #then a number, then a space
+                                                #then a vextor and then a weight.
+                                                # So we will get
+                                                # severak parts to our split
+                                                #a space, a string that is a 
+                                                #number, then a second string 
+                                                #that is a number, etc. 
+           #print("split_string1",split_string)
+           R[i][0] = float(split_string[1])
+           R[i][1] = float(split_string[2])
+           R[i][2] = float(split_string[3])
+           D[i] = float(split_string[4])
+           #print("loop end")
+           #still need 
+        #print("after loop")        
+        read_data = f.readline()            # read a whole line
+        #print("after loop",read_data)
+        temp = float(read_data)
+        #print(temp)
+        read_data = f.readline()            # read a whole line
+        #print(read_data)
+        factor = float(read_data) 
+        #print(factor)
+   #print("return to main program")
+   return NUSE,com,wave,alpha,beta,gamma,psi,RAD,ER,EI,TD,R,D
 ###############################################################################
 #
 ###############################################################################
@@ -170,7 +190,8 @@ def move1kv(mcount,khatN,kth,kph,NK,iseed,divd):
 ###############################################################################
 #
 ###############################################################################
-def read_kv(kth,kph, workfile):
+
+def read_kv(workfile):
    #workfile ="Test_2_5_5.kv"
    with open(workfile) as f:
       read_data = f.readline()            # read a whole line
@@ -214,7 +235,7 @@ def read_kv(kth,kph, workfile):
       mcount=int(split_string[25])
       kcount=int(split_string[36])
       #print(ERR, ERRlast, mcount, kcount)
-      return NK
+      return NK,kth,kph
 
 # now we have the kth and kph values in arrays just like the original
 # code wanted.
@@ -252,16 +273,17 @@ RAD = 0.0                # is the particle semimajor axis (use microns)
 ER = 0.0                 # the complext components of the particle permitivity
 EI = 0.0
 TD = 0.0                 # the dipole size
-R = np.zeros((NMAX,3))    # a NUSE by 3 array of cell locations
+R = np.zeros((NMAX,3))    # a NUSE bcom,wave,alpha,beta,gamma,psi,RAD,ER,EI,TD,R,Dy 3 array of cell locations
 D = np.zeros(NMAX)         # a NUSE array of cell weights
 workfile = "test_output_file.out" # @@@ needs to be an input
-read_input_file(workfile, comments, NUSE, wave, alpha,beta,gamma,psi,RAD, ER, EI, TD, R, D )
-print(coments)
-print(NUSE)
-print(wave, alpha, beta, gamma, psi, RAD)
-print(ER, EI, TD)
-print(R)
-print(D)
+[NUSE,comments,wave,alpha,beta,gamma,psi,RAD,ER,EI,TD,R,D] = read_input_file(workfile)
+# Test to see if it worked
+#print(comments)
+#print(NUSE)
+#print(wave, alpha, beta, gamma, psi, RAD)
+#print(ER, EI, TD)
+#print(R)
+#print(D)
 #
 #
 ###### Read in the K-Vectors
@@ -271,10 +293,10 @@ kph = np.zeros(KMAX)
 # array of k-vector cartisian components.
 khatN = np.zeros((KMAX,3))              # arrat of components of the vectors
 workfile ="Test_2_5_5.kv"   #@@@ needs to be an input
-NK = read_kv(kth,kph, workfile)       # Function call to get the k-vectors
+[NK,kth,kph] = read_kv(workfile)       # Function call to get the k-vectors
 # Test to see if it worked
-# print(NK)
-# for i in range(NK):
+#print(NK)
+#for i in range(NK):
 #    print(kth[i],kph[i])
 
 
