dde66c4ec6d193dc2a568b34fce0e95aab3682f1
rtlines <linest@byui.edu>
2022-02-08 12:10:36 -0700

    Fixed functions to to explicit return of variables.  Python doesn't support pass by reference.

--- pycode/read_kv_2.py
+++ pycode/read_kv_2.py
@@ -29,16 +29,19 @@ import random
 #       return
 #       end
 
-def kvectors4(N,khatN, kth, kph, NK):
-    # Now we need to find the components. A function called kvectors4 does that
-    # in the original code. 
-    # This code just does the calculation of one set of components of one 
-    # vector.  The Nth vector. The compoments are placed in khatN, and array
-    # of vector compoments.  The dimensions of khatN are [NK] by [3].
+def kvector_components(theta, phi):
+    # Function to find the components of a unit vector given in three dimensions
+    #   given the sherical componets angles theta and phi
     #
-    khatN[N,0] = np.sin(kth[N])*np.cos(kph[N]) 
-    khatN[N,1] = np.sin(kth[N])*np.sin(kph[N])
-    khatN[N,2] = np.cos(kth[N])
+    # We are going to call this using our kth and kph arrars and put the result
+    #    into khatN.  So the call shoudl be something like
+    #    khatN(N)=kvectors_components(kth[N], kph[N])
+    #
+    khat=np.zeros(3)
+    khat[0] = np.sin(theta)*np.cos(phi) 
+    khat[1] = np.sin(theta)*np.sin(phi)
+    khat[2] = np.cos(theta)
+    return khat
     # and we are done making the Nth vector components
     
 #
@@ -89,23 +92,25 @@ def kvectors4(N,khatN, kth, kph, NK):
  
 
 def move1kv(mcount,khatN,kth,kph,NK,iseed,divd):
-    # mcount seems to be a particular k-vector number
-    # khatN seems to be the Cartisian components of the k-vector 
+    # mcount is the index of a particular k-vector number
+    # khatN is the Cartisian components of the k-vector 
     # kth and kph are our angle arrays we input above
     # NK is the number of k-vectors
     # iseed is a randum number seed (do this later)
-    # divid seems to be the amount we are dividing pi by to get our increment
+    # divid is the amount we are dividing pi by to get our increment
+    #
     # start by defining an angular increment abount int the theta direction
-    
-    THinc = np.pi/divd      #Theta increment
-    PHinc = 2*THinc         #Phi direction increment is twice as big
-    temp1 = THinc*random.uniform(-1,1)
-    temp2 = PHinc*random.uniform(-1,1)
-    #print(THinc, PHinc, temp1,temp2)
-    kth[mcount] = kth[mcount]+ temp1
-    kph[mcount] = kph[mcount] + temp2
-    
-    # we have a new k-vector angle, but the change ould make us get strange
+    #
+    THinc = np.pi/divd                 #Theta increment
+    PHinc = 2*THinc                    #Phi direction increment is twice as big
+    temp1 = THinc*random.uniform(-1,1) # this is our theta delta
+    temp2 = PHinc*random.uniform(-1,1) # this is our phi delta
+    #
+    kth[mcount] = kth[mcount]+ temp1   #Add the delta theta to the partucuar
+                                       #  theta value
+    kph[mcount] = kph[mcount] + temp2  #Add the delta phi to the partucuar
+                                       #   phi value
+    # we have a new k-vector angle, but the change could make us get strange
     # vectors. We want positive angles, but we also want to adjust the angles
     # either in the positive or the negative direction. This sometimes could 
     # make negagive vector angles. Check for this and correct it if it happens
@@ -119,8 +124,10 @@ def move1kv(mcount,khatN,kth,kph,NK,iseed,divd):
     if (kph[mcount] < 0.0):
         kph[mcount] = -kph[mcount]
     # Now we need to find the components. A function called kvectors4 does that
-    # in the original code. 
-    kvectors4(mcount, khatN, kth, kph, NK)
+    # in the original code. kvectors4(mcount, khatN, kth, kph, NK)
+    #
+    khatN[mcount]=kvector_components(kth[mcount], kph[mcount])
+    #
     #or we could just do it here
     # we should worry about whether we are starting arrays at 0 or 1!!!!! @@@@
     #khatN[mcount,0] = np.sin(kth[mcount])*np.cos(kph[mcount]) 
@@ -129,6 +136,7 @@ def move1kv(mcount,khatN,kth,kph,NK,iseed,divd):
     #
     # and we are done. We have a set of k-vectors with one vector randomly
     # moved.
+    return kth, kph, khatN 
     
     
 
@@ -187,21 +195,20 @@ def move1kv(mcount,khatN,kth,kph,NK,iseed,divd):
 #
 # This is going to be a pain, because we don't have json to help us
 #    We will need some formatted input. 
-import numpy as np
-
-
-import matplotlib.pyplot as plt
+###############################################################################
 #
-workfile ="Test_2_5_5.kv"
-with open(workfile) as f:
-     read_data = f.readline()            # read a whole line
-     #print(read_data)                    # The first line is just the number of kvectors
-     NK=int(read_data)                   # turn the whole line into a number
-     #print(NK)
-     kth = np.zeros(NK)                  # make arrays for the kvector components
-     kph = np.zeros(NK)
-     khatN = np.zeros((NK,3))              # actual components of the vectors
-     for i in range(0,NK,1):             # loop to input all the theta and phi values
+###############################################################################
+def read_kv(workfile):
+   #workfile ="Test_2_5_5.kv"
+   with open(workfile) as f:
+      read_data = f.readline()            # read a whole line
+      #print(read_data)                    # The first line is just the number of kvectors
+      NK=int(read_data)                   # turn the whole line into a number
+      #print(NK)
+#      kth = np.zeros(NK)                  # make arrays for the kvector components
+#      kph = np.zeros(NK)
+#      khatN = np.zeros((NK,3))              # actual components of the vectors
+      for i in range(0,NK,1):             # loop to input all the theta and phi values
           read_data = f.readline()       # read in a whole line
           #print(read_data)
           split_string = read_data.split(" ")  #parse the line splitting where
@@ -225,16 +232,90 @@ with open(workfile) as f:
           kph[i] = float(phi)          # and do the same for the second string
           #print(kth[i],kph[i])
           
-     read_data = f.readline()       # The last line has dummy strings
-                                    # for kth and kph but has at the end 
-     #print(read_data)
-     split_string = read_data.split(" ")
-     #print(split_string)
-     ERR=int(split_string[7])
-     ERRlast=int(split_string[14])
-     mcount=int(split_string[25])
-     kcount=int(split_string[36])
-     #print(ERR, ERRlast, mcount, kcount)
+      read_data = f.readline()       # The last line has dummy strings
+                                     # for kth and kph but has at the end 
+      #print(read_data)
+      split_string = read_data.split(" ")
+      #print(split_string)
+      ERR=int(split_string[7])
+      ERRlast=int(split_string[14])
+      mcount=int(split_string[25])
+      kcount=int(split_string[36])
+      #print(ERR, ERRlast, mcount, kcount)
+      return NK,kth,kph
+
+# now we have the kth and kph values in arrays just like the original
+# code wanted.
+###############################################################################
+#
+###############################################################################
+
+
+
+import numpy as np
+
+######
+#
+KMAX = 2000      # max number of kvectors
+import matplotlib.pyplot as plt
+#
+workfile ="Test_2_5_5.kv"
+###### Read in the K-Vectors
+NK=0
+kth = np.zeros(KMAX)                  # make arrays for the kvector components
+kph = np.zeros(KMAX)
+# array of k-vector cartisian components.
+khatN = np.zeros((KMAX,3))              # arrat of components of the vectors
+workfile ="Test_2_5_5.kv"   #@@@ needs to be an input
+[NK,kth,kph] = read_kv(workfile)       # Function call to get the k-vectors
+# Test to see if it worked
+#print(NK)
+#for i in range(NK):
+#    print(kth[i],kph[i])
+#
+#with open(workfile) as f:
+#     read_data = f.readline()            # read a whole line
+#     #print(read_data)                    # The first line is just the number of kvectors
+#     NK=int(read_data)                   # turn the whole line into a number
+#     #print(NK)
+#     kth = np.zeros(NK)                  # make arrays for the kvector components
+#     kph = np.zeros(NK)
+#     khatN = np.zeros((NK,3))              # actual components of the vectors
+#     for i in range(0,NK,1):             # loop to input all the theta and phi values
+#          read_data = f.readline()       # read in a whole line
+#          #print(read_data)
+#          split_string = read_data.split(" ")  #parse the line splitting where
+#                                               #there are spaces
+#
+#                                               #Each line starts with a sppace
+#                                               #then a number, then a space
+#                                               #then a number. So we will get
+#                                               #three parts to our split
+#                                               #a space, a string that is a 
+#                                               #number, then a second string 
+#                                               #that is a number.
+#          #print(split_string)
+#          theta=split_string[1]        # ignore the first split piece and 
+#                                       # copy the first number string into 
+#                                       # a variable
+#          phi=split_string[2]          # and do the same for the second number
+#                                       # string
+#          #print(theta, phi)     
+#          kth[i] = float(theta)        # now convert the string into a number
+#                                       # and put it into the array
+#          kph[i] = float(phi)          # and do the same for the second string
+#          #print(kth[i],kph[i])
+#          
+#     read_data = f.readline()       # The last line has dummy strings
+#                                    # for kth and kph but has at the end 
+#     #print(read_data)
+#     split_string = read_data.split(" ")
+#     #print(split_string)
+#     ERR=int(split_string[7])
+#     ERRlast=int(split_string[14])
+#     mcount=int(split_string[25])
+#     kcount=int(split_string[36])
+#     #print(ERR, ERRlast, mcount, kcount)
 
 # now we have the kth and kph values in arrays just like the original
 # code wanted.
